8ef284e3dc484fd70a131887635f12020d5bbb80
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2025-12-13 12:47:00 +0300

    1

---
+++ lvl3/lvl3.3/task1/script.js
@@ -0,0 +1,3 @@
+let a = ['dxczv', 'asdcxz', 'sds', 'sd']
+
+console.log(a.filter((element)=>{if(element.length <=3) return element}))
---
+++ lvl3/lvl3.3/task2/script.js
@@ -0,0 +1,6 @@
+let a = 1357
+let res = 'YES'
+
+a.map((element)=>{if(element%2==0) res='NO'})
+
+console.log(res)
