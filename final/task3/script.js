abd7aa37e188600cae8501e5fd9514b0448320bc
fixbananchik <128502663+fixbananchik@users.noreply.github.com>
2025-12-27 13:15:26 +0300

    1

--- final/task3/script.js
+++ final/task3/script.js
@@ -1,4 +1,4 @@
 let a = 'ё3т21л4541т34'
-let nums = [1,2,3,4,5,6,7,8,9,0]
+let nums = ['1','2','3','4','5','6','7','8','9','0']
 
-console.log(a.split('').map((el)=>{return el in nums}).sort((a,b)=>a-b))
+console.log(a.split('').filter((el)=>{return el in nums}).sort((a,b)=>a-b)).join(' ')
